87abf5850d397ab1ba545541237272f65f7e596b
Styff <66780656+styffstyff@users.noreply.github.com>
2023-03-18 20:34:01 +0100

    Update person.py

--- person.py
+++ person.py
@@ -4,9 +4,6 @@ from random import randint
 from scipy import ndimage
 import matplotlib.pyplot as plt
 
-# Objectif creer un objet person qui permette d'accéder
-# Plus simplement à toutes les caractéristiques nécessaire et minimiser 
-# les calculs
 
 class Person():
     def __init__(self, box, frame, mask=None):
@@ -22,22 +19,30 @@ class Person():
     def __str__(self):
         return f"""hBox:{self.hBox} \nheadCenter:{self.headCenter} \nneckCenter:{self.neckCenter} \ntheta:{self.theta} \n"""
 
+    def setBox(self, box):
+        self.box = box
+
     def setHBox(self):
         x, y, w, h = self.box
         # On obtient les boites h1,h2,h3:
         self.hBox = divideBoundingBox(x, y, w, h)
     
-    def displayBox(self, size=2, nbBox=3):
+    def displayBox(self, size=2, color=(0, 255, 0), nbBox=3):
         boxList=self.hBox
         if nbBox == 3:
             x, y, w, h = self.box
-            cv2.putText(self.frame,f"id:{self.getId()}",(x,int(y+3*h)),cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5,(0, 255, 255), 1)
-            cv2.rectangle(self.frame, (x, y), (x+w, y+h), (255, 0, 0), size)
+            cv2.putText(self.frame,f"id:{self.getId()}",(x,int(y+3*h)),cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5,(0,255,255), 1)
+            cv2.rectangle(self.frame, (x, y), (x+w, y+h), color, size)
         else:
             for box in boxList:
                 x, y, w, h = box
-                cv2.rectangle(self.frame, (x, y), (x+w, y+h), (0, 255, 0), size)
-            cv2.putText(self.frame,f"id:{self.getId()}",(x,y+h),cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5,(0, 255, 255), 1)
+                cv2.rectangle(self.frame, (x, y), (x+w, y+h), color, size)
+            cv2.putText(self.frame,f"id:{self.getId()}",(x,y+h),cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5,(0,255,255), 1)
+        return None
+
+    def setHead(self, pos):
+        x, y = pos
+        self.headCenter = x, y
         return None
 
     def setHeadNeckAndArms(self):
@@ -56,6 +61,13 @@ class Person():
             xlen, ylen =np.shape(croppedFrame2)          
         # On calcul la projection du nb de pixel sur y   
 
+        hxlen, hylen = np.shape(hCroppedFrame)
+        # utiliser ceci pour supprimer des filtres accrochés à rien
+        # if (hxlen*hylen - cv2.countNonZero(hCroppedFrame))*100/(hxlen*hylen) > 95 :
+        #     print("IL N'A quasiment RIEN")
+        #     print( (hxlen*hylen - cv2.countNonZero(hCroppedFrame))*100/(hxlen*hylen))
+        #     print(self.box[:2:])
+
         yprojection = np.zeros(ylen)
         color=0
         for idx in range(xlen):
@@ -73,8 +85,21 @@ class Person():
         upperb = np.array(255, np.uint8)
         # lowerb = np.array([0, 0, 0], np.uint8)
         # upperb = np.array([255, 255, 255], np.uint8)
-        # print(hCroppedFrame)
-        hFrame = cv2.inRange(hCroppedFrame, lowerb, upperb)
+        
+        # Bug lié aux prédictions sans détection
+        # if np.shape(hCroppedFrame)[1] == 0 or np.shape(hCroppedFrame)[0] == 0:
+        #     print("Error")
+        #     return None
+            
+        if len(hCroppedFrame) < 1:
+            print("error")
+            return None
+         
+        try: 
+            hFrame = cv2.inRange(hCroppedFrame, lowerb, upperb)
+        except:
+            print("error ??!!")
+            return None
         blobs = hFrame > 100
         labels, nlabels = ndimage.label(blobs)
         # find the center of mass of each label
@@ -85,7 +110,7 @@ class Person():
         center0 = int(cx), int(cy)
         if center0 is not None:
             cv2.circle(hCroppedFrame, (center0[1],center0[0]), 2, 100 , 2)
-            cv2.imshow("cf", hCroppedFrame)
+            # cv2.imshow("cf", hCroppedFrame) DEGUG
             
             self.headCenter = x+center0[1], y+center0[0]
             cv2.circle(self.frame, (self.headCenter), 2, (0, 255, 0), 2)
@@ -96,8 +121,10 @@ class Person():
         # CALCULS POUR LE COU
         if self.headCenter is not None:
             # NOTE la valeur 1.18 est completement arbitraire et 
-            # à modifier avec l'expérience
-            sigma = -1
+            # à modifier avec l'expérience, je pense qu'il faudrait utiliser une 
+            # projection sur les x de maniere similaire à celle sur les y puis 
+            # identifier le minimum pour obtenir la seconde coordonnée du cou
+        
             # neckCenter = (self.headCenter[0]+ sigma*int(self.headCenter[0]*0.02), 400+ int(maxy))
             neckCenter = x+np.argmax(yprojection), int(self.headCenter[1]*1.03)
             # neckCenter = 27,30
@@ -133,6 +160,8 @@ class Person():
             if y2 != miny:
                 theta =  np.arctan(x2/(abs(y2-miny)))
             self.theta = theta
+        
+        return None
 
     def setId(self, id):
         self.id = id
@@ -149,9 +178,12 @@ class Person():
         H1=self.getHbox(0)
         minY = H1[1]
         maxY = minY+int(1.50*H1[3])
-        for y in range(minY, maxY):
-            if (self.frame[y][x] == np.array([255, 255, 255])).any() :
-                return y
+        for y in range(minY, maxY-1):  #IndexError: index 210 is out of bounds for axis 0 with size 210
+            try:
+                if (self.frame[y][x] == np.array([255, 255, 255])).any() :
+                    return y
+            except IndexError:
+                print("person.py ligne 186")
         return maxY
     
     def getId(self):
@@ -159,17 +191,22 @@ class Person():
     
     def getMesure(self):
         x, y = self.headCenter
-        w, h = tuple(self.box[2::])
+        w, h = tuple(self.getBox()[2::])
         return np.array([[x], [y], [w], [h]])
 
 
 class Track():
-    def __init__(self, box, dt):
+    # TODO 
+    # implementer un calcul de l'estimation de l'erreur
+    # Root Mean Squared Error (RMSE)
+    #
+    def __init__(self, box, dt, headCenter=None): 
         self.box = box
         self.dt = dt
-        x, y, w, h = box 
+        _, _, w, h = box 
+        x, y = headCenter
         # Vecteur d'état
-        # x, y, w. h, vx, vy, ax, ay .. je devrais peut-etre ajouter la qté d'acc
+        # x, y, w. h, vx, vy, ax, ay .. je devrais ajouter la qté d'acc
         self.state = np.matrix([[x],[y],[w],[h],[0],[0],[0],[0]])
         # Matrice de transition
         self.transition = np.matrix([   [1, 0, 0, 0, self.dt, 0,       0.5*(self.dt)**2, 0],
@@ -189,7 +226,7 @@ class Track():
         # Matrices de bruit (supposé gaussien):
 
         # bruit relatif à l'évolution de l'objet
-        v=1E-5
+        v=1E-7
 
         self.objectNoise=np.matrix([[v, 0, 0, 0, 0, 0, 0, 0],
                                     [0, v, 0, 0, 0, 0, 0, 0],
@@ -208,10 +245,10 @@ class Track():
                                 [0, 0, 0, v]])
         
         # covariance de l'erreur de la prediction
-        self.P=np.matrix([[500, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 500, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 10, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 10, 0, 0, 0, 0],
+        self.P=np.matrix([[100, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 100, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 5, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 5, 0, 0, 0, 0],
                         [0, 0, 0, 0, 10000, 0, 0, 0],
                         [0, 0, 0, 0, 0, 10000, 0, 0],
                         [0, 0, 0, 0, 0, 0, 10000, 0],
@@ -226,10 +263,11 @@ class Track():
         return self.state
     
     def update(self, z):
+        # z: np.array([[x], [y], [w], [h]])
         # Calcul du gain de Kalman
         S=np.dot(np.dot(self.observation, self.P), self.observation.T)+self.camNoise
         K=np.dot(np.dot(self.P, self.observation.T), np.linalg.inv(S))
-        # Correction 
+        # Correction / innovation
         ecart = z - np.dot(self.observation, self.state)
         self.state=np.round(self.state+np.dot(K, ecart))
         I=np.eye(self.observation.shape[1])
@@ -239,6 +277,7 @@ class Track():
     
     def printing(self):
         print(self.state)
+
 
 def divideBoundingBox(x, y, w, h):
     boxHeight = (h//3)
